63bc937c8340ca1a73d06012d384f2b8104f66b4
belle9217 <33712122+belle9217@users.noreply.github.com>
2024-12-16 19:37:48 +0800

    Create run_clm_sft_dcformer.py

---
+++ pytorch/dcformer/run_clm_sft_dcformer.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Fine-tuning the library models for causal language modeling (GPT, GPT-2, CTRL, ...) on a text file or a dataset.
+
+Here is the full list of checkpoints on the hub that can be fine-tuned by this script:
+https://huggingface.co/models?filter=text-generation
+"""
+
+
+import logging
+import math
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+from pathlib import Path
+import datasets
+import torch
+from build_dataset import build_instruction_dataset, DataCollatorForSupervisedDataset
+import transformers
+from transformers import (
+    CONFIG_MAPPING,
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils import send_example_telemetry
+from transformers.utils.versions import require_version
+
+from configuration_dcformer import DCFormerConfig
+from modeling_dcformer_last_bf16_add_dtype_github import DCFormer
+from safetensors.torch import load_file
+import re
+pattern = re.compile(r'\d+\.bin$')
+
+# import os
+# os.environ['TORCH_LOGS'] = "+dynamo"
+# os.environ['TORCHDYNAMO_VERBOSE'] = "1"
+# import torch._dynamo
+# torch._dynamo.config.suppress_errors = True
+
+IGNORE_INDEX = -100
+
+
+
+def rename_columns(example):   #features: ['input_ids', 'labels'],
+    example["idx"] = example.pop("input_ids")
+    return example
+
+def compile_model(
+    model,
+) -> None:
+    backend = os.environ.get("TORCH_COMPILE_BACKEND", "inductor")
+    for m in reversed(list(model.modules())):
+        m.compile(backend=backend)
+
+
+def load_dcformer(checkpoint_path, config=None, device='cpu',  batch_size=1):
+
+    dcformer = DCFormer(config)
+
+    state_dict = {}
+
+    for filename in os.listdir(checkpoint_path):
+        # print(f)
+        if filename.endswith('model.bin') or pattern.search(filename):
+            file_path = os.path.join(checkpoint_path, filename)
+            state_dict.update(torch.load(file_path))
+        elif filename.endswith('.safetensors'):
+            file_path = os.path.join(checkpoint_path, filename)
+            state_dict.update(load_file(file_path))
+
+    dcformer.load_state_dict(state_dict, strict=False)
+
+    dcformer = dcformer.to(device)
+
+    dcformer.setup_caches(max_batch_size=batch_size)
+    return dcformer
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
+    """
+
+    model_name_or_path: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "The model checkpoint for weights initialization.Don't set if you want to train a model from scratch."
+            )
+        },
+    )
+    tokenizer_name_or_path: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "The tokenizer for weights initialization.Don't set if you want to train a model from scratch."
+            )
+        },
+    )
+
+    config_overrides: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "Override some existing default config settings when a model is trained from scratch. Example: "
+                "n_embd=10,resid_pdrop=0.2,scale_attn_weights=false,summary_type=cls_index"
+            )
+        },
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
+    )
+    use_fast_tokenizer: bool = field(
+        default=True,
+        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
+    )
+    model_revision: str = field(
+        default="main",
+        metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
+    )
+    use_auth_token: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Will use the token generated when running `huggingface-cli login` (necessary to use this script "
+                "with private models)."
+            )
+        },
+    )
+    torch_dtype: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "Override the default `torch.dtype` and load the model under this dtype. If `auto` is passed, the "
+                "dtype will be automatically derived from the model's weights."
+            ),
+            "choices": ["auto", "bfloat16", "float16", "float32"],
+        },
+    )
+
+    def __post_init__(self):
+        if self.config_overrides is not None and (self.config_name is not None or self.model_name_or_path is not None):
+            raise ValueError(
+                "--config_overrides can't be used in combination with --config_name or --model_name_or_path"
+            )
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    dataset_dir: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+
+    train_file: Optional[str] = field(default=None, metadata={"help": "The input training data file (a text file)."})
+    validation_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "An optional input evaluation data file to evaluate the perplexity on (a text file)."},
+    )
+
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+    validation_split_percentage: Optional[float] = field(
+        default=0.05,
+        metadata={
+            "help": "The percentage of the train set used as validation set in case there's no validation split"
+        },
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+    keep_linebreaks: bool = field(
+        default=True, metadata={"help": "Whether to keep line breaks when using TXT files or not."}
+    )
+    data_cache_dir: Optional[str] = field(default=None, metadata={"help": "The datasets processed stored"})
+
+    max_seq_length: Optional[int] = field(default=512)
+
+
+@dataclass
+class MyTrainingArguments(TrainingArguments):
+    force_resize_embeddings: bool = field(default=False)
+    q_chunk_size : Optional[int] = field(default=64)
+    compile : Optional[bool] = field(default=False)
+    small : Optional[bool] = field(default=False)
+    use_gradient_checkpointing : Optional[bool] = field(default=True)
+
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, MyTrainingArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    send_example_telemetry("run_clm", model_args, data_args)
+
+    # Setup logging
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,  # if training_args.local_rank in [-1, 0] else logging.WARN,
+        handlers=[logging.StreamHandler(sys.stdout)],)
+
+
+    if training_args.should_log:
+        # The default of training_args.log_level is passive, so we set log level at info here to have that default.
+        transformers.utils.logging.set_verbosity_info()
+
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+    config = DCFormerConfig.from_pretrained(model_args.model_name_or_path)
+
+    if training_args.small:
+        config.n_layer = 8
+
+    config.is_training = training_args.do_train  #True
+    config.use_gradient_checkpointing = training_args.use_gradient_checkpointing  #True
+    config.q_chunk_size = training_args.q_chunk_size
+    
+    print(config)
+
+
+    tokenizer_kwargs = {
+        "cache_dir": model_args.cache_dir,
+        "use_fast": model_args.use_fast_tokenizer,
+        "revision": model_args.model_revision,
+        "use_auth_token": True if model_args.use_auth_token else None,
+        "trust_remote_code": True
+    }
+    tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name_or_path, **tokenizer_kwargs)
+    tokenizer.clean_up_tokenization_spaces = True
+
+
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    eval_dataset=None
+    train_dataset = None
+
+    if training_args.do_train:
+        with training_args.main_process_first(desc="loading and tokenization"):
+            path = Path(data_args.dataset_dir)
+            files = [os.path.join(path,file.name) for file in path.glob("*.json")]
+            logger.info(f"Training files: {' '.join(files)}")
+            train_dataset = build_instruction_dataset(
+                data_path=files,
+                tokenizer=tokenizer,
+                max_seq_length=data_args.max_seq_length,
+                data_cache_dir = None,
+                preprocessing_num_workers = data_args.preprocessing_num_workers)
+        logger.info(f"Num train_samples  {len(train_dataset)}")
+        logger.info("training example:")
+        logger.info(tokenizer.decode(train_dataset[0]['input_ids']))
+    if training_args.do_eval:
+        with training_args.main_process_first(desc="loading and tokenization"):
+            files = [data_args.validation_file]
+            logger.info(f"Evaluation files: {' '.join(files)}")
+            eval_dataset = build_instruction_dataset(
+                data_path=files,
+                tokenizer=tokenizer,
+                max_seq_length=data_args.max_seq_length,
+                data_cache_dir = None,
+                preprocessing_num_workers = data_args.preprocessing_num_workers)
+        logger.info(f"Num eval_samples  {len(eval_dataset)}")
+        logger.info("eval example:")
+        logger.info(tokenizer.decode(eval_dataset[0]['input_ids']))
+
+
+
+    model = load_dcformer(model_args.model_name_or_path, config=config, device=training_args.device, batch_size=training_args.per_device_train_batch_size)
+
+    if training_args.compile:
+        compile_model(model)
+    
+    train_dataset = train_dataset.map(rename_columns)
+    if eval_dataset is not None:
+        eval_dataset = eval_dataset.map(rename_columns)
+    try:
+        train_dataset = train_dataset.remove_columns("input_ids")
+        eval_dataset = eval_dataset.remove_columns("input_ids")
+    except:
+        pass
+
+    # train_dataset = train_dataset.select(range(6))
+    # eval_dataset = eval_dataset.select(range(6))
+
+    # Initialize our Trainer
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
+    # Training
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+
+        metrics = train_result.metrics
+
+        metrics["train_samples"] = len(train_dataset)
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+
+        metrics = trainer.evaluate()
+        metrics["eval_samples"] =len(eval_dataset)
+        try:
+            perplexity = math.exp(metrics["eval_loss"])
+        except OverflowError:
+            perplexity = float("inf")
+        metrics["perplexity"] = perplexity
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+
+if __name__ == "__main__":
+    main()
